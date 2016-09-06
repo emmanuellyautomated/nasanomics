cbcd0057cc48a9427ebc8764d47c1b460b1b1320
Emmanuel Obi <emmanuel@ivan.local>
2016-09-06 09:20:33 -0500

    add filebeat setup; nix printouts

--- vps/vagrant/provision.sh
+++ vps/vagrant/provision.sh
@@ -1,5 +1,13 @@
-# Setup Python3, etc.
+cd /vagrant
+sudo echo "deb https://packages.elastic.co/beats/apt stable main" |  sudo tee -a /etc/apt/sources.list.d/beats.list
 sudo apt-get -y update
+
+# Setup Filebeat
+sudo apt-get -y install apt-transport-https filebeat
+sudo wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+#---- Configure Filebeat
+
+# Setup Python3, etc.
 sudo apt-get -y install python3.4-dev python3-pip virtualenv git libevent-dev
 sudo echo "alias python=\"python3\"" >> ~vagrant/.bashrc
 source ~vagrant/.bashrc
@@ -9,13 +17,13 @@ mkdir /var/www
 mkdir /var/www/nasanomics
 export PYTHONPATH=/var/www/nasanomics
 /bin/bash -c "rsync -av \
-    --exclude='/vagrant/nasanomics/vps/packer' \
-    --exclude='/vagrant/nasanomics/vps/vagrant' \
-    /vagrant/nasanomics/ /var/www/nasanomics"
+    --exclude='/vagrant/vps/packer' \
+    --exclude='/vagrant/vps/vagrant' \
+    /vagrant/ /var/www/nasanomics"
 
 # Setup virtualenv
 cd /var/www/nasanomics
-sudo make venv
+make venv
 source /var/www/nasanomics/venv/bin/activate
 make install
 
@@ -27,14 +35,14 @@ touch /var/log/uwsgi/emperor.log
 chown www-data:www-data /var/log/uwsgi/emperor.log
 touch /var/log/uwsgi/app/nasanomics.log
 chown www-data:www-data /var/log/uwsgi/app/nasanomics.log
-cp /vagrant/nasanomics/vps/vagrant/uwsgi.conf /etc/init
-cp /vagrant/nasanomics/vps/vagrant/uwsgi_config.ini /etc/uwsgi/apps-available/
+cp /vagrant/vps/vagrant/uwsgi.conf /etc/init
+cp /vagrant/vps/vagrant/uwsgi_config.ini /etc/uwsgi/apps-available/
 ln -s /etc/uwsgi/apps-available/uwsgi_config.ini /etc/uwsgi/apps-enabled
 
 # NGINX Setup
 sudo apt-get -y install nginx
 rm /etc/nginx/sites-enabled/default
-cp /vagrant/nginx_config /etc/nginx/sites-available/
+cp /vagrant/vps/vagrant/nginx_config /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/nginx_config /etc/nginx/sites-enabled
 
 # Start UWSGI, NGINX, and Redis
